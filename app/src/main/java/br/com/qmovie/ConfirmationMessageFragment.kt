0dc5d3ae04a35766b4ebfbe911cbf28e402272ad
Luiz Yamaoka <luiz.yamaoka@gmail.com>
2020-11-15 19:03:11 -0300

    adiciona clique no x do dialog

--- app/src/main/java/br/com/qmovie/ConfirmationMessageFragment.kt
+++ app/src/main/java/br/com/qmovie/ConfirmationMessageFragment.kt
@@ -1,25 +1,16 @@
 package br.com.qmovie
 
-import android.app.Dialog
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.DialogFragment
-import kotlinx.android.synthetic.main.fragment_confirmation_message.*
 
 class ConfirmationMessageFragment : DialogFragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -39,11 +30,9 @@ class ConfirmationMessageFragment : DialogFragment() {
                 view.findViewById<TextView>(R.id.tvConfirmacaoMensagem).text = getString(R.string.dialog_confirmacao_desistir_mensagem)
             }
         }
-        return view
-    }
 
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        return super.onCreateDialog(savedInstanceState)
+        view.findViewById<Button>(R.id.btnFecharConfirmacao).setOnClickListener { this.dismiss() }
+        return view
     }
 
 }
