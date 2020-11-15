eb4a95b8cf094db894527017425e971bef85e5a7
Guilherme Nogueira <guilherme.nogueira11@fatec.sp.gov.br>
2020-11-15 16:14:15 -0300

    testes

--- app/src/main/java/br/com/qmovie/MainActivity.kt
+++ app/src/main/java/br/com/qmovie/MainActivity.kt
@@ -1,8 +1,10 @@
 package br.com.qmovie
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.fragment.NavHostFragment
+import kotlinx.android.synthetic.main.menubar.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,6 +13,12 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
 
-        navController.navigate(R.id.jogoFragment)
+        navController.navigate(R.id.novoJogoFragment)
+
+        menuJogo.setOnClickListener {
+            var intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
