c91496936b3c7efb19294b03f0c5d798d80c6e99
Luiz Yamaoka <luiz.yamaoka@gmail.com>
2020-11-22 18:51:11 -0300

    arrumar conexoes entre telas

--- app/src/main/java/br/com/qmovie/activity/MainActivity.kt
+++ app/src/main/java/br/com/qmovie/activity/MainActivity.kt
@@ -3,35 +3,38 @@ package br.com.qmovie.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.fragment.NavHostFragment
-import br.com.qmovie.NovoJogoFragment
-import br.com.qmovie.R
-import br.com.qmovie.RankingFragment
+import br.com.qmovie.*
 import kotlinx.android.synthetic.main.menubar.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-//        val navController = navHostFragment.navController
-//
-//        navController.navigate(R.id.jogoFragment)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        when (intent.getStringExtra("destinationFragment")) {
+            "NovoJogoFragment" -> navController.navigate(R.id.novoJogoFragment)
+            "RankingFragment" -> navController.navigate(R.id.rankingFragment)
+            "LancamentosFragment" -> navController.navigate(R.id.lancamentosFragment)
+            "ConfiguracoesFragment" -> navController.navigate(R.id.configuracoesFragment)
+            else -> navController.navigate(R.id.novoJogoFragment)
+        }
 
         menuJogo.setOnClickListener {
-            var fragNovoJogo =
-                NovoJogoFragment.newInstance()
-            supportFragmentManager.beginTransaction().apply {
-                add(R.id.nav_host_fragment, fragNovoJogo)
-                commit()
-            }
+            navController.navigate(R.id.novoJogoFragment)
         }
 
         menuRank.setOnClickListener {
-            var fragRank = RankingFragment.newInstance()
-            supportFragmentManager.beginTransaction().apply {
-                replace(R.id.nav_host_fragment, fragRank)
-                commit()
-            }
+            navController.navigate(R.id.rankingFragment)
+        }
+
+        menuLanca.setOnClickListener {
+            navController.navigate(R.id.lancamentosFragment)
+        }
+
+        menuConfi.setOnClickListener {
+            navController.navigate(R.id.configuracoesFragment)
         }
 
     }
