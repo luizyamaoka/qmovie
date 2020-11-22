c020173c28623fbab00db37d0a60e00abae766b3
Luiz Yamaoka <luiz.yamaoka@gmail.com>
2020-11-22 14:06:34 -0300

    refactor MainActivity package

--- app/src/main/java/br/com/qmovie/activity/MainActivity.kt
+++ app/src/main/java/br/com/qmovie/activity/MainActivity.kt
@@ -1,9 +1,11 @@
-package br.com.qmovie
+package br.com.qmovie.activity
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.fragment.NavHostFragment
+import br.com.qmovie.NovoJogoFragment
+import br.com.qmovie.R
+import br.com.qmovie.RankingFragment
 import kotlinx.android.synthetic.main.menubar.*
 
 class MainActivity : AppCompatActivity() {
@@ -16,7 +18,8 @@ class MainActivity : AppCompatActivity() {
         navController.navigate(R.id.jogoFragment)
 
         menuJogo.setOnClickListener {
-            var fragNovoJogo = NovoJogoFragment.newInstance()
+            var fragNovoJogo =
+                NovoJogoFragment.newInstance()
             supportFragmentManager.beginTransaction().apply {
                 add(R.id.nav_host_fragment, fragNovoJogo)
                 commit()
