0b16ef6451ada5227a8e312b241db43df7da5956
Luiz Yamaoka <luiz.yamaoka@gmail.com>
2021-01-30 18:23:22 -0300

    renomeia variaveis

--- app/src/main/java/br/com/qmovie/extension/LongExtension.kt
+++ app/src/main/java/br/com/qmovie/extension/LongExtension.kt
@@ -1,16 +1,15 @@
 package br.com.qmovie.extension
 
 import java.text.NumberFormat
-import java.util.*
 
 fun Long.toTime() : String {
     val minutos = (this / 1000) / 60
     val segundos = (this / 1000) % 60
 
-    val segundos_string = if (segundos < 10) "0${segundos}" else "${segundos}"
-    val minutos_string = if (minutos < 10) "0${minutos}" else "${minutos}"
+    val segundosString = if (segundos < 10) "0${segundos}" else "${segundos}"
+    val minutosString = if (minutos < 10) "0${minutos}" else "${minutos}"
 
-    return "$minutos_string:$segundos_string"
+    return "$minutosString:$segundosString"
 }
 
 fun Long.toPoints() = "${NumberFormat.getNumberInstance().format(this)} p"
