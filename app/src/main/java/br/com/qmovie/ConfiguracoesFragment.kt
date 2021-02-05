ae9de74cbabd2c7702aaa63da7e1b8aa06c7d375
Luiz Yamaoka <luiz.yamaoka@gmail.com>
2021-02-05 19:59:20 -0300

    arrumar botoes das configuracoes

--- app/src/main/java/br/com/qmovie/ConfiguracoesFragment.kt
+++ app/src/main/java/br/com/qmovie/ConfiguracoesFragment.kt
@@ -8,14 +8,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import br.com.qmovie.activity.LoginActivity
 import br.com.qmovie.activity.MainActivity
 import br.com.qmovie.viewmodel.UserViewModel
 import br.com.qmovie.viewmodel.viewModelFactory
 import com.google.firebase.auth.FirebaseAuth
-import kotlinx.android.synthetic.main.custom_preference2.btnLogout
 
 
 class ConfiguracoesFragment : PreferenceFragmentCompat() {
@@ -54,5 +55,23 @@ class ConfiguracoesFragment : PreferenceFragmentCompat() {
         val view: View? = super.onCreateView(inflater, container, savedInstanceState)
         view!!.setBackgroundColor(Color.rgb(25, 26, 29))
         return view
+    }
+
+    override fun onPreferenceTreeClick(preference: Preference?): Boolean {
+        when (preference!!.key) {
+            requireContext().getString(R.string.sair) -> {
+                viewModel.logout()
+                val intent = Intent(activity, LoginActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+            requireContext().getString(R.string.contato) -> {
+                Toast.makeText(activity, "Aplicativo desenvolvido por Guilherme Nogueira, Luiz Yamaoka e Vitor Gabriel. Para entrar em contato utilze o email contato@qmovie.com.br", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            else -> {
+                return false
+            }
+        }
     }
 }
