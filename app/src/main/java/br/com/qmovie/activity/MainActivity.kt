91285ac193b0bad5f70212056583bad9684122bd
Guilherme <guilherme.nogueira11@fatec.sp.gov.br>
2021-02-04 23:24:01 -0300

    no_connection

--- app/src/main/java/br/com/qmovie/activity/MainActivity.kt
+++ app/src/main/java/br/com/qmovie/activity/MainActivity.kt
@@ -1,6 +1,9 @@
 package br.com.qmovie.activity
 
+import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.fragment.NavHostFragment
@@ -12,6 +15,7 @@ import kotlinx.android.synthetic.main.menubar.*
 class MainActivity : AppCompatActivity() {
 
     private var user : FirebaseUser? = null
+    var isConnected : Boolean = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,20 +23,35 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
 
+        isOnline()
         when (intent.getStringExtra("destinationFragment")) {
             "NovoJogoFragment" -> navController.navigate(R.id.novoJogoFragment)
             "RankingFragment" -> navController.navigate(R.id.rankingFragment)
             "LancamentosFragment" -> navController.navigate(R.id.lancamentosFragment)
             "ConfiguracoesFragment" -> navController.navigate(R.id.configuracoesFragment)
-            else -> navController.navigate(R.id.novoJogoFragment)
+            "NoConnectionFragment" -> navController.navigate(R.id.noConnectionFragment)
+            else -> if (isConnected){
+                navController.navigate(R.id.novoJogoFragment)
+            } else {
+                navController.navigate(R.id.lancamentosFragment)
+            }
         }
 
         menuJogo.setOnClickListener {
-            navController.navigate(R.id.novoJogoFragment)
+            isOnline()
+            if(isConnected) {
+                navController.navigate(R.id.novoJogoFragment)
+            } else{
+                navController.navigate(R.id.noConnectionFragment)
+            }
         }
 
         menuRank.setOnClickListener {
-            navController.navigate(R.id.rankingFragment)
+            if(isConnected) {
+                navController.navigate(R.id.rankingFragment)
+            } else{
+                navController.navigate(R.id.noConnectionFragment)
+            }
         }
 
         menuLanca.setOnClickListener {
@@ -53,5 +72,11 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun isOnline(){
+        val connectivityManager : ConnectivityManager = this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork : NetworkInfo? = connectivityManager.activeNetworkInfo
+        isConnected  = activeNetwork?.isConnectedOrConnecting == true
     }
 }
